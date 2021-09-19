4350b2a654ccc56aadb71ebc0268f7e648bcd5b1
Rusty Roberts <rust.roberts@protonmail.com>
2021-09-19 14:27:32 -0700

    minimum viable pong

--- pong/Ball.lua
+++ pong/Ball.lua
@@ -1,10 +1,16 @@
 Ball = {}
 
+BALL_INIT_SPEED = 300
+
 function Ball:new(t)
     t = t or {}
     self.x = 0
     self.y = 0
+    self.dx = math.random(2) == 1 and -BALL_INIT_SPEED or BALL_INIT_SPEED
+    self.dy = math.random(2) == 1 and math.random(0, -BALL_INIT_SPEED) or
+              math.random(0, BALL_INIT_SPEED)
     self.radius = 10
+    -- TODO: actually implement color
     self.color = {1, 1, 1}
     self.__index = self
     setmetatable(t, self)
@@ -25,8 +31,16 @@ function Ball:setX(x)
     self.x = x
 end
 
+function Ball:setDx(dx)
+    self.dx = dx
+end
+
 function Ball:setY(y)
     self.y = y
+end
+
+function Ball:setDy(dy)
+    self.dy = dy
 end
 
 function Ball:getColor()
@@ -41,12 +55,49 @@ function Ball:getX()
     return(self.x)
 end
 
+function Ball:getDx()
+    return(self.dx)
+end
+
 function Ball:getY()
     return(self.y)
 end
 
+function Ball:getDy()
+    return(self.dy)
+end
+
 function Ball:draw()
     love.graphics.circle("fill", self:getX(), self:getY(), self:getRadius())
+end
+
+function Ball:move(dt)
+    self:setX(self:getX() + self:getDx() * dt)
+    self:setY(self:getY() + self:getDy() * dt)
+end
+
+function Ball:isColliding(paddle)
+    if
+        self:getX() > paddle:getX() + paddle:getWidth() or
+        paddle:getX() > self:getX() + self:getRadius() then
+        return false
+    elseif
+        self:getY() > paddle:getY() + paddle:getHeight() or
+        paddle:getY() > self:getY() + self:getRadius() then
+        return false
+    else
+        return true
+    end
+end
+
+-- put the ball back
+function Ball:set()
+    -- I don't like using these globals from main...
+    self:setX(WINDOW_WIDTH / 2)
+    self:setY(WINDOW_HEIGHT / 2)
+    self:setDx(math.random(2) == 1 and -BALL_INIT_SPEED or BALL_INIT_SPEED)
+    self:setDy(math.random(2) == 1 and math.random(0, -BALL_INIT_SPEED) or
+               math.random(0, BALL_INIT_SPEED))
 end
 
 return(Ball)
--- pong/main.lua
+++ pong/main.lua
@@ -4,13 +4,38 @@ local Ball = require"Ball"
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-PADDLE_SPEED = 200  -- px/dt
+PADDLE_SPEED = 300  -- px/dt
+WINNING_SCORE = 11
 
 function love.load()
+    -- seed the RNG so that calls to random are randomish
+    math.randomseed(os.time())
 
     function DrawGameState()
         love.graphics.printf(
-            {{1, 0, 0}, GameState}, Fonts.small, 0, 0, WINDOW_WIDTH
+            {{0.3, 0.3, 0.3}, "Game state: " .. GameState},
+            Fonts.small, 0, 0, WINDOW_WIDTH
+        )
+    end
+
+    function DisplayFPS()
+        love.graphics.printf(
+            {{0.3, 0.3, 0.3}, tostring("FPS: " .. love.timer.getFPS())},      -- text
+            Fonts.small,          -- font
+            0,                  -- x
+            WINDOW_HEIGHT - Fonts.small:getHeight(),  -- y
+            WINDOW_WIDTH,       -- sx
+            "center"            -- align
+        )
+    end
+
+    function DrawScore()
+        love.graphics.printf(
+            tostring(
+                "Player 1: " .. Player1Score .. "\n" ..
+                "Player 2: " .. Player2Score
+            ),
+            Fonts.small, 0, 0, WINDOW_WIDTH, "center"
         )
     end
 
@@ -36,7 +61,7 @@ function love.load()
     Player2 = Paddle:new{}
     Player2:setX(WINDOW_WIDTH - Player2:getWidth())
     Player2:setY(WINDOW_HEIGHT / 2 - Player2:getHeight() / 2)
-    Ball = Ball:new()
+    Ball = Ball:new{}
 end
 
 function love.draw()
@@ -48,7 +73,7 @@ function love.draw()
             0,                  -- x
             WINDOW_HEIGHT / 2,  -- y
             WINDOW_WIDTH,       -- sx
-            'center'            -- align
+            "center"            -- align
         )
         love.graphics.printf(
             {{0, 1, 0}, "Press 'Enter' to begin."},
@@ -56,40 +81,96 @@ function love.draw()
             0,
             WINDOW_HEIGHT / 2 + Fonts.big:getHeight(),
             WINDOW_WIDTH,
-            'center'
+            "center"
         )
-        DrawGameState()
+        Player1Score = 0
+        Player2Score = 0
+        Ball:set()
     elseif GameState == "serve" then
         -- start of a volley
+        DrawScore()
         Player1:draw()
         Player2:draw()
-        Ball:draw(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
-        DrawGameState()
+        love.graphics.printf(
+            "Press Enter to serve!",
+            Fonts.big,
+            0,
+            WINDOW_HEIGHT / 2,
+            WINDOW_WIDTH,
+            "center"
+        )
+    elseif GameState == "play" then
+        DrawScore()
+        Ball:draw()
+        Player1:draw()
+        Player2:draw()
+    elseif GameState == "finished" then
+        love.graphics.printf(
+            {{0, 1, 0}, GameWinner .. " wins!"},
+            Fonts.big, 0, WINDOW_HEIGHT / 2, WINDOW_WIDTH, "center"
+        )
     end
 
-    -- always show hint for quitting
+    -- always show hint for quitting, game state, and fPS
     love.graphics.printf(
-        {{0.3, 0.3, 0.3}, "Press 'Esc' to quit."},
+        {{1, 0, 0}, "Press 'Esc' to quit."},
         Fonts.small, 0, 0, WINDOW_WIDTH, "right"
     )
+    DrawGameState()
+    DisplayFPS()
 
 end
 
 function love.update(dt)
-    -- if GameState == "start" then
-    -- elseif GameState == "serve" then
-    -- end
+    if GameState == "serve" then
+        return
+    elseif GameState == "play" then
+        Ball:move(dt)
+    end
+
+    -- check for collisions w/ paddles
+    if Ball:isColliding(Player1) or Ball:isColliding(Player2) then
+        Sounds.paddle:play()
+        Ball:setDx(-Ball:getDx() * 1.1)
+        Ball:setDy(Ball:getDy() + math.random(-100, 100))
+    end
+
+    -- ball to the walls
+    if Ball:getY() < Ball:getRadius() then
+        Ball:setY(Ball:getRadius())
+        Ball:setDy(-Ball:getDy())
+    elseif Ball:getY() > WINDOW_HEIGHT - Ball:getRadius() then
+        Ball:setY(WINDOW_HEIGHT - Ball:getRadius())
+        Ball:setDy(-Ball:getDy())
+    -- player2 scores on player1
+    elseif Ball:getX() < Ball:getRadius() then
+        Player2Score = Player2Score + 1
+        Ball:set()
+        if Player2Score == WINNING_SCORE then
+            GameState = "finished"
+            GameWinner = "Player 2"
+        else
+            GameState = "serve"
+        end
+    -- player1 scores on player2
+    elseif Ball:getX() > WINDOW_WIDTH - Ball:getRadius() then
+        Player1Score = Player1Score + 1
+        Ball:set()
+        if Player1Score == WINNING_SCORE then
+            GameState = "finished"
+            GameWinner = "Player 1"
+        else
+            GameState = "serve"
+        end
+    end
 
     -- paddles can move whenever
-    if love.keyboard.isDown("s") then
+    if love.keyboard.isDown("d") then
         Player1:setVelocity(PADDLE_SPEED)
-        print("Player 1 Y: " .. Player1:getY())
-    elseif love.keyboard.isDown("d") then
+    elseif love.keyboard.isDown("s") then
         Player1:setVelocity(-PADDLE_SPEED)
-        print("Player 1 Y: " .. Player1:getY())
     else
         Player1:setVelocity(0)
-        print("Player 1 Y: " .. Player1:getY())
     end
 
     -- paddles can move whenever
@@ -106,9 +187,15 @@ function love.update(dt)
 end
 
 function love.keypressed(key)
-    if key == "enter" or key == "return" and GameState == "start" then
-        GameState = "serve"
-        Sounds.paddle:play()
+    if key == "enter" or key == "return" then
+        if GameState == "start" then
+            GameState = "serve"
+        elseif GameState == "serve" then
+            Sounds.paddle:play()
+            GameState = "play"
+        elseif GameState == "finished" then
+            GameState = "start"
+        end
     elseif key == "escape" then
         love.event.quit()
     end
